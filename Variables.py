a57debad4e846f3444b4d027d569a5b70e8409ed
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-03-14 00:01:14 +0700

    Compare variables in C++ and Python

---
+++ Variables.py
@@ -0,0 +1,18 @@
+print("I installed Python today!")
+
+gas = 10
+# int gas = 10;
+
+rent = 100.5
+# double rent = 100.5
+
+house = 100000000000000000000000000000000
+# long long house = 100000000000000000000000000000000
+
+print(gas + rent)
+print(gas + rent + house)
+
+item1 = "Sugar"
+item2 = 'a'
+item3 = "Ice"
+print(item1,item2,item3)
