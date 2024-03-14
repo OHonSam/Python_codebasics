bf9966cdd1790d1499c5db174da02f76f13064ab
OHonSam <ohsam22@apcs.fitus.edu.vn>
2024-03-15 00:04:03 +0700

    Basic String Operations in Python

---
+++ String.py
@@ -0,0 +1,12 @@
+item = "ice cream"
+
+# You cannot change the str[idx] compared to C++
+
+
+# substr(str.begin()+i,str.begin()+j) 
+print(item[:3])
+print(item[4:9])
+print(item[6:])
+
+# convert number into string using str()
+print(item + " mlem " + str(25))
